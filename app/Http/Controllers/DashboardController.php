eb2664527eff656c33c4a23b75c6e27aaccdffda
GoglichidzeMike <goglichidze.mike@gmail.com>
2021-04-25 23:06:14 +0400

    controllers reerangement from blog to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 class DashboardController extends Controller
+
 {
     public function __construct()
     {  
@@ -16,4 +19,112 @@ class DashboardController extends Controller
     {
         return view('dashboard');
     }
+
+
+    /// for dashboard
+
+    public function store(Request $request)
+    {
+    $this->validate($request, [
+        'body'=> 'required',
+        'name'=> 'required',
+        'slug'=> 'required',
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    ]);
+
+    //get image, set destination path, change image name to current time in secs and save it.
+    $image = $request->file('image');
+    $destinationPath = 'uploads/image/';
+    $filename = date('YmdHis') . "." . $image->getClientOriginalExtension();
+    $image->move($destinationPath, $filename);
+
+    $request->user()->blogs()->create([
+        'body'=> $request->body,
+        'name'=> $request->name,
+        'slug'=> $request->slug,
+        'image'=> $filename
+    ]);
+
+        return redirect()->route('blog.dashboard')->with('status', 'Blog created successfully');
+    }
+
+
+    //blog dashboard index
+
+
+    public function blog_dashboard_index()
+    {
+        $blogs = Blog::orderBy('created_at', 'desc')->paginate(20); 
+        
+        return view('dashboard.blogs.index', [ 
+        'blogs' => $blogs
+        ]);
+    }
+
+    //blog dashboard create page
+    public function blog_create()
+    {
+        return view('dashboard.blogs.create');
+    }
+
+    //blog dashboard show
+
+    public function blog_show($slug)
+    {
+
+        $blog = Blog::where('slug', '=', $slug)->firstOrFail();
+
+        return view('dashboard.blogs.show',[
+            'blog' => $blog
+        ]);
+    }
+
+
+    //blog dashboard edit
+    public function blog_edit($id)
+    {
+        $blog = Blog::find($id);
+        return view('dashboard.blogs.edit', ['blog' => $blog]);
+    }
+
+    //blog dashboard update
+
+    public function blog_update(Request $request, $id)
+    {
+        $blog = Blog::find($id);
+        $blog->name = $request->name;
+        $blog->body = $request->body;
+
+        if($request->hasFile('image'))
+        {
+            File::delete('uploads/image/'.$blog->image);
+            $image = $request->file('image');
+            $destinationPath = 'uploads/image/';
+            $filename = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $filename);
+            $blog->image = $filename;
+        }
+
+        $blog->save();
+
+        return redirect()->route('blog.show', $blog->slug)->with('status', 'Blog updated successfully');
+    }
+
+    //blog dashboard delete
+
+    public function blog_delete($id)
+    {
+        $blog = Blog::find($id);
+        $blog->delete();
+        
+        File::delete('uploads/image/'.$blog->image);
+
+        return redirect()->route('blog.dashboard')->with('status', 'Blog deleted successfully');
+    }
+
+
+    //blog dashboard 
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,23 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\BlogsController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function(){
     return view('home');
 })->name('home');
 
 Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
-Route::get('/dashboard/blogs', [BlogsController::class,'blog_dashboard_index'])->name('blog.dashboard');
-Route::get('/dashboard/blogs/create', [BlogsController::class,'blog_create'])->name('blog.create');
-Route::get('/dashboard/blogs/{slug}', [BlogsController::class,'blog_show'])->name('blog.show');
-Route::get('/dashboard/blogs/edit/{id}', [BlogsController::class,'blog_edit'])->name('blog.edit');
-Route::post('/dashboard/blogs/update/{id}', [BlogsController::class,'blog_update'])->name('blog.update');
-Route::post('/dashboard/blogs/{id}', [BlogsController::class,'blog_delete'])->name('blog.destroy');
+Route::get('/dashboard/blogs', [DashboardController::class,'blog_dashboard_index'])->name('blog.dashboard');
+Route::get('/dashboard/blogs/create', [DashboardController::class,'blog_create'])->name('blog.create');
+Route::get('/dashboard/blogs/{slug}', [DashboardController::class,'blog_show'])->name('blog.show');
+Route::get('/dashboard/blogs/edit/{id}', [DashboardController::class,'blog_edit'])->name('blog.edit');
+Route::post('/dashboard/blogs/update/{id}', [DashboardController::class,'blog_update'])->name('blog.update');
+Route::post('/dashboard/blogs/{id}', [DashboardController::class,'blog_delete'])->name('blog.destroy');
+Route::post('/blogs', [DashboardController::class,'store']);
+
+
 
 Route::get('/blogs', [BlogsController::class,'index'])->name('blogs');
-Route::post('/blogs', [BlogsController::class,'store']);
 
 
 
